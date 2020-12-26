93555f93d47fe61fe92b7e49812d7b65ead7c9b2
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-26 06:12:33 -0600

    Create the project

---
+++ src/Injection.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Thomas Tuegel
+SPDX-License-Identifier: BSD-3-Clause
+Maintainer: Thomas Tuegel <ttuegel@mailbox.org>
+
+Canonical injections and projections.
+-}
+
+module Injection
+    ( projectName
+    ) where
+
+
+projectName :: String
+projectName = "injection"
