15e8be6e1ae5fa5891a88c4837fd7413b534d684
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-28 06:01:42 -0600

    Clean up test names

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,24 +12,24 @@ import Test.QuickCheck.Instances ()
 
 main :: IO ()
 main = hspec $ do
-    describe "retract :: Integer -> Dynamic" $ do
+    describe "retract @Integer @Dynamic" $ do
         it "is the left inverse of inject" (lawLeftInverse @Integer @Dynamic)
-    describe "inject :: Maybe Integer -> [Integer]" $ do
+    describe "inject @(Maybe Integer) @[Integer]" $ do
         it "outputs a value with the same length as the input" $ do
             let test = inject @(Maybe Integer) @[Integer]
             length (test Nothing) `shouldBe` 0
             length (test (Just 1)) `shouldBe` 1
         it "is injective" (lawInjective @(Maybe Integer) @[Integer])
-    describe "retract :: [Integer] -> Maybe (Maybe Integer)" $ do
+    describe "retract @(Maybe Integer) @[Integer]" $ do
         it "is the left inverse of inject" (lawLeftInverse @(Maybe Integer) @[Integer])
-    describe "inject :: Natural -> Integer" $ do
+    describe "inject @Natural @Integer" $ do
         it "injects the value itself" $ do
             let test = inject @Natural @Integer
             test 0 `shouldBe` 0
             test 1 `shouldBe` 1
             test 2 `shouldBe` 2
         it "is injective" (lawInjective @Natural @Integer)
-    describe "retract :: Integer -> Maybe Natural" $ do
+    describe "retract @Natural @Integer" $ do
         it "is the left inverse of inject" (lawLeftInverse @Natural @Integer)
         it "is defined over non-negative integers" $ do
             retract @Natural @Integer 0 `shouldBe` Just 0
