8ff5bc993139d8eb3c0328df844ea22c3aa90f5e
Thomas Tuegel <ttuegel@mailbox.org>
2021-04-03 06:46:57 -0500

    shell.nix: Use nix-haskell-hls

--- shell.nix
+++ shell.nix
@@ -5,11 +5,10 @@ let
   inherit (default) shellFor;
 
   sources = import ./nix/sources.nix;
-  haskell-language-server = import sources."haskell-language-server-nix" {
-    ghc-versions = [ "8.10.2" ];
-  };
+  nix-haskell-hls = import sources."nix-haskell-hls" { ghcVersion = "ghc8104"; };
+  inherit (nix-haskell-hls) hls-renamed;
 in
 
 shellFor {
-  buildInputs = [ haskell-language-server ];
+  buildInputs = [ hls-renamed ];
 }
